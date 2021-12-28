226aa81cb031a0e8bb1f3f1afa1aa24e193b4c71
Semen Lunin <leynelss@gmail.com>
2021-12-29 00:58:52 +0300

    Update base_page.py

--- part4/pages/base_page.py
+++ part4/pages/base_page.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from .locators import BasePageLocators
 import math
 
